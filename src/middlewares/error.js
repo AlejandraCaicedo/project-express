afe8266f043fa184aac4e41d7f4959eb46dda947
AlejandraCaicedo <caicedomaria905@gmail.com>
2022-04-06 18:19:56 -0500

    New express project

---
+++ src/middlewares/error.js
@@ -0,0 +1,10 @@
+const error = (err, res, req, next) => {
+  const errorCode = err.status || 500;
+  return res.status(errorCode).send({
+    status: errorCode,
+    uri: req,
+    message: err.message || "Internal server error",
+  });
+};
+
+module.exports = error;
